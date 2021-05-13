e903e67c8e66e87931797c7cba1e6bd929c39e9b
lukeren314 <lukeren314@gmail.com>
2021-05-12 19:59:31 -0700

    fixed server route for index

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use("/api", routes);
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 // main index.html
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 app.listen(port, () => console.log(`Server Start! Listening on port ${port}`));
